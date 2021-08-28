f6ff589161e5f0638dff3de26eb887dc4ead8711
Fernando De León Gil <gasnare@hotmail.com>
2021-08-28 14:11:59 -0500

    add to the calculator result to the different operator

--- lib/controllers/calculator_controller.dart
+++ lib/controllers/calculator_controller.dart
@@ -1,9 +1,9 @@
 import 'package:get/get.dart';
 
 class CalculatorController extends GetxController {
-  var firstNumber = '10'.obs;
-  var secondNumber = '20'.obs;
-  var mathResult = '30'.obs;
+  var firstNumber = '0'.obs;
+  var secondNumber = '0'.obs;
+  var mathResult = '0'.obs;
   var operator = '+'.obs;
 
   resetAll() {
@@ -38,6 +38,53 @@ class CalculatorController extends GetxController {
       return mathResult.value = '0.';
     } else {
       mathResult.value = mathResult.value + '.';
+    }
+  }
+
+  selectOperation(String operation) {
+    operator.value = operation;
+    firstNumber.value = mathResult.value;
+    mathResult.value = '0';
+  }
+
+  deletedLastEntry() {
+    if (mathResult.value.replaceAll('-', '').length > 1) {
+      mathResult.value = mathResult.substring(0, mathResult.value.length - 1);
+    } else {
+      mathResult.value = '0';
+    }
+  }
+
+  calculateResult() {
+    double num1 = double.parse(firstNumber.value);
+    double num2 = double.parse(mathResult.value);
+
+    secondNumber.value = mathResult.value;
+
+    switch (operator.value) {
+      case '+':
+        mathResult.value = '${num1 + num2}';
+        break;
+
+      case '-':
+        mathResult.value = '${num1 - num2}';
+        break;
+
+      case 'x':
+        mathResult.value = '${num1 * num2}';
+        break;
+
+      case '/':
+        mathResult.value = '${num1 / num2}';
+        break;
+
+      default:
+        return;
+    }
+
+    if (mathResult.value.endsWith('.0')) {
+      mathResult.value =
+          mathResult.value.substring(0, mathResult.value.length - 2);
     }
   }
 }
